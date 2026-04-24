ebb24e2ef867c9251603113ad47cf9e92a22d119
Nicolas <ncg0612@gmail.com>
2026-04-23 22:56:49 -0500

    ajuste para variables de entorno

--- consumer-app/src/api/client.ts
+++ consumer-app/src/api/client.ts
@@ -1,10 +1,10 @@
-import axios from 'axios'
+import axios, { InternalAxiosRequestConfig } from 'axios'
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000'
-})
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000'
 
-api.interceptors.request.use((config) => {
+const api = axios.create({ baseURL: API_URL })
+
+api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   const token = localStorage.getItem('token')
   if (token) {
     config.headers['Authorization'] = `Bearer ${token}`
--- delivery-app/src/api/client.ts
+++ delivery-app/src/api/client.ts
@@ -1,11 +1,17 @@
 import axios, { InternalAxiosRequestConfig } from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:4000' })
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000'
+
+const api = axios.create({ baseURL: API_URL })
 
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   const token = localStorage.getItem('token')
-  if (token) config.headers.Authorization = `Bearer ${token}`
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`
+  }
   return config
+}, (error) => {
+  return Promise.reject(error)
 })
 
 export default api
